8d062a69118399f69691483e69a6fffbe71813d7
htooaungkhant <htooaungkhant321580@gmail.com>
2024-04-09 01:06:58 +0630

    [ADD] Button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
-  const [ form, setForm] = useState({name : '', email: '', message: ''})
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
   const handleChange = () => {};
+  const handleFocus = () => {};
+  const handelBlur = () => {};
+  const handleSubmit = () => {};
+
   return (
-    <section className='relative flex lg:flex-row flex-col max-container'>
-      <div className='flex-1 min-w -[50%] flex flex-col'>
-        <h1 className='head-text'>Get in touch</h1>
-        <form className='w-full flex flex-col gap-7 mt-14'>
-          <label className='text-black-500 font-semibold'>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w -[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black-500 font-semibold">
             Name
-            <input 
-            type='text' 
-            name='name' 
-            className='input' 
-            placeholder='Htoo Aung Khant' 
-            required 
-            value={form.name}
-            onChange={handleChange}
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Your Name"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handelBlur}
             />
           </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="your@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handelBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              className="input"
+              placeholder="Let me know how I can help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handelBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disable={isLoading}
+            onFocus={handleFocus}
+            onBlur={handelBlur}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
